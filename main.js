f775b147452014c5f7abcda1f89b496c4d1cc396
Роман Колеров <coder8080@mail.ru>
2021-03-27 21:01:57 +0300

    update

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+/* Система заметок */
+
+// Получение зависимостей
 const express = require('express')
 const express_session = require('express-session')
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite3')
 const operations = require('./my_modules/user_operations')
 
+// Создание переменных
 const app = express()
 const db = new sqlite.Database('db.sqlite3')
 const urlEncodedParser = bodyParser.urlencoded({extended: false})
 
+// Настройка
 app.set('view engine', 'ejs')
 app.use(express_session({secret: 'super_secret_key'}))
 app.use('/public', express.static('public'))
 
+// Отслеживание url адресов
+
+/* Главная страница */
 app.get('/', ((req, res) => {
     if (req.session.logged) {
         res.redirect('/profile')
@@ -20,6 +28,7 @@ app.get('/', ((req, res) => {
     }
 }))
 
+/* Страница входа */
 app.get('/login', ((req, res) => {
     if (req.session.logged) {
         res.redirect('/profile')
@@ -28,18 +37,21 @@ app.get('/login', ((req, res) => {
     }
 }))
 
+/* Вход */
 app.post('/login', urlEncodedParser, ((req, res) => {
     operations.log(req, res)
 }))
 
+/* Профиль */
 app.get('/profile', ((req, res) => {
     if (req.session.logged) {
-        db.get(`select heading, notes.id from notes inner join users on notes.userId = users.id where users.login = '${req.session.login}';`, (err, data) => {
+        db.all(`select heading, notes.id from notes inner join users on notes.userId = users.id where users.login = '${req.session.login}';`, (err, data) => {
             if (err) {
                 throw err
             }
             let notes_arr
             let are_notes
+            console.log(data)
             if (data) {
                 if (data[0]) {
                     are_notes = true
@@ -58,19 +70,25 @@ app.get('/profile', ((req, res) => {
     }
 }))
 
+/* Страница записи */
 app.get('/note/:id', ((req, res) => {
+    // Пропускаем только вошедших пользователей
     if (req.session.logged) {
+        // Получаем логин пользователя, которому принадлежит запись
         db.get(`select users.login from notes inner join users on notes.userId = users.id where notes.id = '${req.params.id}';`, (err, data) => {
             if (err) {
                 throw err
             }
             if (data) {
+                // Проверяем, принадлежит ли запись запрашивающему её пользователю
                 if (data.login === req.session.login) {
+                    // Получаем запись и отправляем её пользователю
                     db.get(`select * from notes where notes.id = '${req.params.id}';`, (err, data) => {
                         console.log(data)
                         if (data) {
                             res.render('note', {note: data})
                         } else {
+                            // Если запись вдруг исчезла, выдаём ошибку
                             res.render('msg', {msg: 'Ошибка'})
                         }
                     })
@@ -86,19 +104,24 @@ app.get('/note/:id', ((req, res) => {
     }
 }))
 
+/* Страница изменения записи */
 app.get('/change-note/:id', ((req, res) => {
     if (req.session.logged) {
+        // Получаем логин пользователя, которому принадлежит запись
         db.get(`select users.login from notes inner join users on notes.userId = users.id where notes.id = '${req.params.id}';`, (err, data) => {
             if (err) {
                 throw err
             }
             if (data) {
+                // Проверяем, принадлежит ли запись запрашивающему её пользователю
                 if (data.login === req.session.login) {
+                    // Получаем запись и отправляем её пользователю
                     db.get(`select * from notes where notes.id = '${req.params.id}';`, (err, data) => {
                         console.log(data)
                         if (data) {
                             res.render('change-note', {note: data})
                         } else {
+                            // Если запись вдруг исчезла, выдаём ошибку
                             res.render('msg', {msg: 'Ошибка'})
                         }
                     })
@@ -116,18 +139,22 @@ app.get('/change-note/:id', ((req, res) => {
 
 app.post('/change-note/:id', urlEncodedParser, (req, res) => {
     if (req.session.logged) {
+        // Получаем логин пользователя, которому принадлежит запись
         db.get(`select users.login from notes inner join users on notes.userId = users.id where notes.id = '${req.params.id}';`, (err, data) => {
             if (err) {
                 throw err
             }
             if (data) {
+                // Проверяем, принадлежит ли запись запрашивающему её пользователю
                 if (data.login === req.session.login) {
+                    // Получаем запись и отправляем её пользователю
                     db.get(`select heading, text from notes where id = ${req.params.id};`, (err, data) => {
                         if (err) {
                             throw err
                         }
                         if (data) {
                             if (data.heading !== req.body.heading) {
+                                // Если в название были внесены изменения, то сохраняем их
                                 db.run(`update notes set heading = '${req.body.heading}' where id = ${req.params.id};`, (err) => {
                                     if (err) {
                                         throw err
@@ -135,6 +162,7 @@ app.post('/change-note/:id', urlEncodedParser, (req, res) => {
                                 })
                             }
                             if (data.text !== req.body.text) {
+                                // Если в текст были внесены изменения, сохраняем их
                                 db.run(`update notes set text = '${req.body.text}' where id = ${req.params.id};`, (err) => {
                                     if (err) {
                                         throw err
@@ -143,6 +171,7 @@ app.post('/change-note/:id', urlEncodedParser, (req, res) => {
                             }
                             res.redirect(`/note/${req.params.id}`)
                         } else {
+                            // Если запись вдруг исчезла, выдаём ошибку
                             res.render('msg', {msg: 'Ошибка'})
                         }
                     })
@@ -151,6 +180,34 @@ app.post('/change-note/:id', urlEncodedParser, (req, res) => {
                 }
             } else {
                 res.render('msg', {msg: 'У вас нет записи с таким титулом'})
+            }
+        })
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/create-note', ((req, res) => {
+    if (req.session.logged) {
+        res.render('new-note')
+    } else {
+        res.redirect('/login')
+    }
+}))
+
+app.post('/create-note', urlEncodedParser, (req, res) => {
+    if (req.session.logged) {
+        db.get(`select id from users where login = '${req.session.login}';`, (err, data) => {
+            if (err) {
+                throw err
+            }
+            if (data) {
+                console.log()
+                db.run(`insert into notes (heading, text, userId) values ('${req.body.heading}', '${req.body.text}', ${data.id})`)
+                res.redirect('/')
+            }
+            else {
+                res.render('msg', {msg: 'Ошибка'})
             }
         })
     } else {
